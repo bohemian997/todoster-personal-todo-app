8a8deda1817b7ef23ca73c95c2cbee1381c2299f
bohemian997 <19mandal97gmail.com>
2020-04-17 20:05:24 +0530

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# todoster-personal-todo-app
-This is my personal todo app developed with Express.Js and Vue.Js
+# Todoster-personal-todo-app
+## Introduction
+This is my personal todo app developed with ReactJs.
+## Usage
+* cd into the project `cd todoster-personal-todo-app`
+* Install dependencies `npm install`
+* run application `npm start`
--- src/Components/Database/todoMonthData.js
+++ src/Components/Database/todoMonthData.js
@@ -2,19 +2,19 @@ const todoofMonth = [{
         id: 1,
         date: new Date(2020, 5, 1),
         deadline: "5 days",
-        todo: ["Wash pant", "wash shirt", "wash face"],
+        todo: ["Wash clothes", "water plants", "bring medicine"],
     },
     {
         id: 2,
         date: new Date(2020, 5, 5),
         deadline: "2 days",
-        todo: ["street light", "break"],
+        todo: ["call shubham", "study algorithms"],
     },
     {
         id: 3,
         date: new Date(2020, 5, 8),
         deadline: "1 hr",
-        todo: ["Wash pant", "bring aloo", "go to doctor"],
+        todo: ["pay electric bill", "go to market", "go to doctor"],
     },
 ]
 
